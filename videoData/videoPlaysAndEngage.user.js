cb51982b53ca58fbde9249afabd9b4255c4667df
Alex Chan <alex.chin1@hotmail.com>
2023-07-07 12:10:14 +0800

    Changed the method of data retrieval from HTML to Web API.

--- videoData/videoPlaysAndEngage.user.js
+++ videoData/videoPlaysAndEngage.user.js
@@ -20,7 +20,7 @@
     let styleNode = document.createElement("style");
     styleNode.setAttribute("type", "text/css");
     styleNode.innerHTML = `
-.video-info-v1 .video-data .video-data-list .item {
+.video-info-container .video-info-detail .video-info-detail-list .item {
 margin-right: 7px;
 }
 .pudate-text {
@@ -85,6 +85,65 @@ color: #222;
         return response.json();
     }
 
+    // https://www.bilibili.com/video/BV19a4y1g7fe
+    const pathname = window.location.pathname;
+    // BV: BV19a4y1g7fe
+    const BV = pathname.split('/')[2]
+
+    async function getVideoData(BVID) {
+        const response = await fetch(`https://api.bilibili.com/x/web-interface/view?bvid=${BVID}`)
+        return response.json();
+    }
+
+    const dataList = document.querySelector('.video-info-detail-list')
+    const videoData = await getVideoData(BV)
+    console.log('view: ', videoData.data)
+
+
+    let titleStr = document.querySelector('h1').title
+    let viewCountNum = videoData.data.stat.view
+    let dmCountNum = videoData.data.stat.danmaku
+    let datetimeStr = '' // 时间
+    let likeCountNum = videoData.data.stat.like // 点赞
+    let coinCountNum = videoData.data.stat.coin // 投币
+    let favoriteCountNum = videoData.data.stat.favorite // 收藏
+    let shareCountNum = videoData.data.stat.share // 分享
+    let commentCountNum = videoData.data.stat.reply // 评论
+    let followerCountNum = 0 // 粉丝数
+
+    console.log('----------------------------------------')
+    console.log('viewCountNum: ', viewCountNum)
+    console.log('dmCountNum: ', dmCountNum)
+    console.log('likeCountNum: ', likeCountNum)
+    console.log('coinCountNum: ', coinCountNum)
+    console.log('collectCountNum: ', favoriteCountNum)
+    console.log('shareCountNum: ', shareCountNum)
+    console.log('commentCountNum: ', commentCountNum)
+
+    let EngageCountNum = dmCountNum + likeCountNum + coinCountNum + favoriteCountNum + shareCountNum + commentCountNum
+
+    console.log('EngageCountNum: ', EngageCountNum)
+
+    const newElement = `<span id="bofang" title="播放" class="item" style="color: #E11"><b>播: ${viewCountNum}</b></span><span id="danmu" title="弹幕" class="item" style="color: #9c27b0"><b>弹幕: ${dmCountNum}</b></span><span id="pinglun" title="评论" class="item" style="color: #2bb291"><b>评: ${commentCountNum}</b></span><span id="hudong" title="互动" class="item" style="color: #007FEC"><b>互动: ${EngageCountNum}</b></span>`
+
+    const timer1 = setInterval(function () {
+        if (refreshCount >= 4) {
+            clearInterval(timer1)
+        }
+        console.log(`--------------------[Start ${refreshCount + 1}]--------------------`)
+
+        if (refreshCount <= 0) {
+            dataList.insertAdjacentHTML('afterbegin', newElement)
+        } else {         
+            const viewEl = document.querySelector('#bofang')
+            if (!viewEl) {
+                dataList.insertAdjacentHTML('afterbegin', newElement)
+            }
+        }
+        refreshCount += 1
+    }, 2000)
+    return
+
     const timer = setInterval(function () {
         // setTimeout(function() {
 
@@ -92,28 +151,17 @@ color: #222;
             clearInterval(timer)
         }
 
-        let titleStr = document.querySelector('h1').title
-        let viewCountNum = 0
-        let dmCountNum = 0
-        let datetimeStr = ''
-        let likeCountNum = 0
-        let coinCountNum = 0
-        let collectCountNum = 0
-        let shareCountNum = 0
-        let commentCountNum = 0
-        let followerCountNum = 0
-
         const upNameElm = document.querySelector('.up-name').childNodes[0];
         const upName = upNameElm.textContent.trim();
 
         const viewElement = document.querySelector('.view.item')
-        const dmElement = document.querySelector('.dm.item')
+        const dmElement = document.querySelector('.dm.item').innerText
         const likeRaw = document.querySelector('.video-like-info.video-toolbar-item-text').innerText
         const coinRaw = document.querySelector('.video-coin-info.video-toolbar-item-text').innerText
         const collectRaw = document.querySelector('.video-fav-info.video-toolbar-item-text').innerText
         const shareRaw = document.querySelector('.video-share-wrap.video-toolbar-left-item').children[0].innerText
 
-        const dataList = document.querySelector('.video-data-list')
+        const dataList = document.querySelector('.video-info-detail-list')
         const commentCountElm = document.querySelector('.total-reply')
 
         // console.log('viewElement:', viewElement)
@@ -125,11 +173,12 @@ color: #222;
         // console.log('commentCountElm: ', commentCountElm)
         console.log(`--------------------[Start ${refreshCount + 1}]--------------------`)
 
-        viewCountNum = getNumber2(viewElement.title)
-        dmCountNum = getNumber2(dmElement.title)
+        // B站不在这个地方放出数据了
+        // viewCountNum = getNumber2(viewElement.title)
+        dmCountNum = getNumber2(dmElement)
         likeCountNum = getCountNum(likeRaw)
         coinCountNum = getCountNum(coinRaw)
-        collectCountNum = getCountNum(collectRaw)
+        favoriteCountNum = getCountNum(collectRaw)
         shareCountNum = getCountNum(shareRaw)
         // commentCountNum = Number(commentCountElm.textContent)
         if (commentCountElm && commentCountElm.textContent) {
@@ -141,14 +190,14 @@ color: #222;
         console.log('dmCountNum: ', dmCountNum)
         console.log('likeCountNum: ', likeCountNum)
         console.log('coinCountNum: ', coinCountNum)
-        console.log('collectCountNum: ', collectCountNum)
+        console.log('collectCountNum: ', favoriteCountNum)
         console.log('shareCountNum: ', shareCountNum)
         console.log('commentCountNum:', commentCountNum)
 
         // viewElement.childNodes[1].textContent = viewCountNum
         // dmElement.childNodes[1].textContent = dmCountNum
 
-        const EngageCountNum = dmCountNum + likeCountNum + coinCountNum + collectCountNum + shareCountNum + commentCountNum
+        const EngageCountNum = dmCountNum + likeCountNum + coinCountNum + favoriteCountNum + shareCountNum + commentCountNum
         console.log('互动数:', EngageCountNum)
 
         // <span id="follow" title="粉丝数" class="item" style="color: #ecd200"><b>互动: ${followerCountNum}</b></span>
